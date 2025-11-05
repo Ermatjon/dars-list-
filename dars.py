f70e847f5258ab81d65605a6fa84d2a4e22c4dc5
Ermatjon <ermatjonshavkatov@mail.ru>
2025-11-05 16:31:23 +0500

    Add files via upload

---
+++ dars.py
@@ -0,0 +1,143 @@
+'''list_0=range(1,51)'''
+list_l=list(map(int,input("sonlarni kiriting: ").split()))
+'11'
+'''
+for i in list_1:
+    if i%2==0:
+        print(i)
+'''
+'12'
+'''
+royxat=[]
+for son in list_1:
+    if son<0:
+        royxat.append(son)
+        print(royxat)
+        print(f"manfiy sonlar {len(royxat)}ta")
+'''
+'13'
+'''
+print(sum(list_1))
+'''
+'14'
+'''
+print(f"eng katta qiymat {max(list_1)}")
+print(f"eng kichik qiymat {min(list_1)}")
+'''
+'16'
+'''
+for son in list_1:
+    son=son*2
+    print(son)
+'''
+'16'
+'''
+for i in list_1:
+    if i%3==0:
+        print(f"{i} soni 3 ga karrali")
+'''
+'17'
+'''
+ok=input("ismingizni kiriting:").lower()
+if ok in ["rover","urinboyuu","urinboyu"]
+    print("Salom ")
+'''
+'18'
+'''
+if 0<list_0 and list_0<10:
+    print(list_0)
+'''
+'19'
+'''
+son=int(input(":"))
+if son in list_0:
+    print(son)
+'''
+'21'
+'''
+if sum(list_1)>100:
+    print("katta")
+else:
+    print("kichik")
+'''
+'23'
+'''
+for i in list_1:
+    if i==0:
+        print("bor")
+    else:
+        print("yoq")
+'''
+'25'
+'''
+list1 = [1, -2, 3, -4, -5, 6, -7, -8, 9, -10]
+list_yangi=[]
+for son in list1:
+    if son<0:
+        list_yangi.append(son)
+print(list_yangi)
+'''
+'26'
+'''
+for list in list_1:
+    if list>5:
+        print(list-2)
+'''
+'27'
+son=list(map(int,input("sonlarni kiriting: ").split()))
+musbat=[]
+for i in son:
+    if i>0:
+        musbat.append(i)
+if musbat==len(son):
+    print("barchasi musbat")
+else:    
+    print("manfiylar ham bor")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'15'
+'''
+sonlar=list(map(int,input("10 ta son kiriting:").split()))
+for son in sonlar:
+    if son>0:
+        print(son)
+'''
+'22'
+'''
+s=0
+sonlar_1=list(map(int,input("5-ta son kiriting:").split())
+for i in sonlar:
+    if i%2==0:
+       s+=i
+print(s)    
+'''
+
+
+
+
+
+
+
+
+              
